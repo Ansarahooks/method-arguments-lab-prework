b8cdc509eb6b1cc684c3e5620a63f17afb605d73
ansara.hooks <ansara.hooks@gmail.com>
2019-02-08 17:38:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,8 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 introduction("Josh")
+
+def introduction (name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction("Josh","Ruby")
